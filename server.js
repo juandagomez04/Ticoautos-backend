845c2c08ff2bfe3e7385cb9a329435815a911499
juandagomez04 <jdgomezcubillo2004@gmail.com>
2026-03-19 03:27:00 -0600

    feat: add inbox routes and increase payload limit to 10mb

--- server.js
+++ server.js
@@ -4,13 +4,19 @@ const cors = require("cors");
 require("dotenv").config();
 
 const authRoutes = require("./routes/auth.routes");
+const vehicleRoutes = require("./routes/vehicle.routes");
+const inboxRoutes = require("./routes/inbox.routes");
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use("/auth", authRoutes);
+app.use("/api/vehicles", vehicleRoutes);
+app.use("/api/inbox", inboxRoutes);
+
 
 app.get("/", (req, res) => {
     res.send("REST API funcionando 🚀");
